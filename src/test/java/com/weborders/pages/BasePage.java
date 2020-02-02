c36fffebfe6b815ec93663c948d69288bf51fe34
inahiddemir <i.nahid.demir@gmail.com>
2020-02-02 02:45:02 -0800

    Executed parallel testing

--- src/test/java/com/weborders/pages/BasePage.java
+++ src/test/java/com/weborders/pages/BasePage.java
@@ -1,10 +1,13 @@
 package com.weborders.pages;
 
+import com.weborders.utilities.BrowserUtils;
 import com.weborders.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public abstract class BasePage {
     /*
@@ -13,6 +16,9 @@ public abstract class BasePage {
      */
     @FindBy(tagName = "h1")
     private WebElement pageLogo;
+
+    @FindBy(tagName = "th")
+    private List<WebElement> columnNamesElemenets;
 
     public BasePage(){
         //its coming from selenium WebDriver
@@ -28,4 +34,7 @@ public abstract class BasePage {
         Driver.get().findElement(By.linkText(moduleName)).click();
     }
 
+    public List<String> getColumnNames(){
+        return BrowserUtils.getListOfString(columnNamesElemenets);
+    }
 }
---
+++ src/test/java/com/weborders/step_definitons/ViewAllOrdersStepDefinitons.java
@@ -0,0 +1,20 @@
+package com.weborders.step_definitons;
+
+import com.weborders.pages.ViewAllOrdersPage;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+import java.util.List;
+
+public class ViewAllOrdersStepDefinitons {
+
+    ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
+
+    @Then("I should be able to see following columns:")
+    public void i_should_be_able_to_see_following_columns(List<String> dataTable) {
+        System.out.println("Expected: "+ dataTable);
+        System.out.println("Actual: "+ viewAllOrdersPage.getColumnNames());
+        Assert.assertEquals(dataTable, viewAllOrdersPage.getColumnNames());
+    }
+
+}
