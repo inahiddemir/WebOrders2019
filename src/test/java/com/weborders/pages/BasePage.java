acc695d15155f828d43ca69a2f63afd5a3d3361e
inahiddemir <i.nahid.demir@gmail.com>
2020-02-01 20:40:14 -0800

    first commit

---
+++ src/test/java/com/weborders/pages/BasePage.java
@@ -0,0 +1,10 @@
+package com.weborders.pages;
+
+public abstract class BasePage {
+
+    /*
+    This class gonna be super class for all page classses. Includuing  all common locators,
+    WebElements and initialize page factory.
+
+     */
+}
