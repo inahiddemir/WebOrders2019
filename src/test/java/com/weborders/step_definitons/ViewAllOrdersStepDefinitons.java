017aaa8ce9d32c38e80a5489f97f1ddd77e663d4
inahiddemir <i.nahid.demir@gmail.com>
2020-02-05 09:02:39 -0800

    Added Jenkins File

--- src/test/java/com/weborders/step_definitons/ViewAllOrdersStepDefinitons.java
+++ src/test/java/com/weborders/step_definitons/ViewAllOrdersStepDefinitons.java
@@ -1,6 +1,7 @@
 package com.weborders.step_definitons;
 
 import com.weborders.pages.ViewAllOrdersPage;
+import com.weborders.utilities.Driver;
 import io.cucumber.java.en.Then;
 import org.junit.Assert;
 
@@ -11,7 +12,8 @@ public class ViewAllOrdersStepDefinitons {
     ViewAllOrdersPage viewAllOrdersPage = new ViewAllOrdersPage();
 
     @Then("I should be able to see following columns:")
-    public void i_should_be_able_to_see_following_columns(List<String> dataTable) {
+    public void i_should_be_able_to_see_following_columns(List<String> dataTable) throws InterruptedException {
+        Driver.get().wait(5);
         System.out.println("Expected: "+ dataTable);
         System.out.println("Actual: "+ viewAllOrdersPage.getColumnNames());
         Assert.assertEquals(dataTable, viewAllOrdersPage.getColumnNames());
